66fbac2e8a0cc5fbf0946f31bee89a139d19e56c
ksqsf <i@ksqsf.moe>
2017-07-20 16:41:40 +0800

    replace getInitialResultSet

--- extension.js
+++ extension.js
@@ -1,53 +1,72 @@
-
-const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const AppDisplay = imports.ui.appDisplay;
+const Search = imports.ui.search;
 
-let text, button;
+const SearchResults = Main.overview._controls.viewSelector._searchResults;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+// NOTE: this is a workaround because GNOME Shell directly registered 
+// 		 AppSearchProvider instead of holding an object
+const AppSearchProvider = SearchResults._providers[0];
+
+///
+/// Local Namespace
+///
+var _PinyinSearch = {}
+
+///
+/// Configuration
+///
+_PinyinSearch.pinyin_search = false // true for using pinyin, false for not
+
+///
+/// Search
+///
+
+// match_substr checks if char vec @needle <= char vec @haystack
+function match_substr(haystack, needle) {
+	var i = 0;
+	for (var j = 0; j < needle.length; ++j) {
+		for (; i < haystack.length; ++i)
+			if (haystack[i] == needle[j])
+				break;
+
+		if (i < haystack.length)
+			continue;
+		else
+			return false;
+	}
+	return true;
 }
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+// This function replaces GNOME Shell's implementation
+_PinyinSearch.getInitialResultSet = function(terms, callback, cancellable) {
 }
 
+///
+/// Extensions
+///
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+	print("pinyinsearch init");
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+	_PinyinSearch._shell_getInitialResultSet = AppSearchProvider.getInitialResultSet;
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+	print("pinyinsearch enabled");
+
+	// Replace GNOME Shell's search
+	AppSearchProvider.getInitialResultSet = _PinyinSearch.getInitialResultSet;
+
+	// Initialize app keys
+	_PinyinSearch.app_keys = {};
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+	print("pinyinsearch disabled");
+
+	// Restore GNOME Shell's search
+	AppSearchProvider.getInitialResultSet = _PinyinSearch._shell_getInitialResultSet;
+
+	// Free
+	delete _PinyinSearch.app_keys;
 }
